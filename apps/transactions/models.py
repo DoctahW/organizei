b24e095f50d671e647e51b7222fe59c0e618c8b7
RiosGabri <grp@cesar.school>
2026-03-28 12:03:53 -0300

    feat: implement Transaction model and views for transaction management

--- apps/transactions/models.py
+++ apps/transactions/models.py
@@ -1,1 +1,20 @@
 from django.db import models
+
+class Transaction(models.Model):
+    # Definição dos tipos
+    TYPE_CHOICES = [
+        ('DEPOSIT', 'Entrada'),
+        ('WITHDRAWAL', 'Saída'),
+    ]
+    # Campos de uma transação (provisórios)
+    name = models.CharField(max_length=150, verbose_name="Descrição")
+    value = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Valor")
+    transaction_type = models.CharField(
+        max_length=10, 
+        choices=TYPE_CHOICES, 
+        verbose_name="Tipo de Transação"
+    ) 
+    date = models.DateField(auto_now_add=True, verbose_name="Data") 
+
+    def __str__(self):
+        return f"{self.name} - R$ {self.value} ({self.get_transaction_type_display()})"
--- apps/transactions/views.py
+++ apps/transactions/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .forms import TransactionForm
+from .models import Transaction
+from django.db.models import Sum, Q
 
 # exibir o formulário para criar uma nova transação
 def create_transactions(request):
@@ -18,3 +20,23 @@ def post_transaction(request):
     else:
         form = TransactionForm()
     return render(request, 'transactions/transactions.html', {'form': form})
+
+# EXIBIR o extrato (entradas e saída) + o saldo 
+def get_transactions(request):
+    transactions = Transaction.objects.all().order_by('-date')
+    # Cálculo do saldo total
+    totais = transactions.aggregate(
+        e=Sum('value', filter=Q(transaction_type='DEPOSIT')), 
+        #soma das entrada 
+        s=Sum('value', filter=Q(transaction_type='WITHDRAWAL')) 
+    )  
+    total_entradas = totais['e'] or 0
+    total_saidas = totais['s'] or 0
+    saldo = total_entradas - total_saidas
+    #saldo = (totais['e'] or 0) - (totais['s'] or 0)
+    return render(request, 'transactions/list_transactions.html', {
+        'transactions': transactions, 
+        'total_entradas': total_entradas, 
+        'total_saidas': total_saidas,     
+        'saldo': saldo                    
+    })
