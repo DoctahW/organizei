a6aa53d123f3c467c0ad8ed0a92f42b247c8cd14
euclides <joaomartires2@gmail.com>
2026-05-17 23:24:06 -0300

    feat: add register, profile and password change views

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -5,6 +5,14 @@ from . import views
 app_name = "accounts"
 
 urlpatterns = [
-    path("login/", views.login_view, name="login"),
+    path("login/", views.auth_view, name="login"),
+    path("register/", views.auth_view, name="register"),
     path("logout/", views.logout_view, name="logout"),
+    path("profile/", views.profile_view, name="profile"),
+    path("password_change/", views.password_change_view, name="password_change"),
+    path(
+        "password_change/done/",
+        views.password_change_done_view,
+        name="password_change_done",
+    ),
 ]
